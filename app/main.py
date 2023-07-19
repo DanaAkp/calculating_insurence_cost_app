a893f381ddd5524849fd2b5700ed1dde5f8816f2
Danagul Akpaeva <Danagul.Akpayeva@waveaccess.ru>
2023-07-19 15:27:41 +0300

    added docker containers for db and app, added file README.md

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import datetime
 from logging.config import dictConfig
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
@@ -5,15 +6,15 @@ from tortoise.contrib.fastapi import register_tortoise
 from app.config import DB_URL, log_config
 from app.routers.tariff_plan import tariff_plan_router
 from app.routers.transfers import transfers_router
+from app.controllers import tariff_plan_service
+from app.models import TariffPlan
 
 
 dictConfig(log_config)
 app = FastAPI(title='CalculatingInsuranceApp')
 
-
 app.include_router(transfers_router)
 app.include_router(tariff_plan_router)
-
 
 register_tortoise(
     app,
@@ -22,3 +23,11 @@ register_tortoise(
     generate_schemas=True,
     add_exception_handlers=True,
 )
+
+
+@app.on_event('startup')
+async def startup_event():
+    if not await TariffPlan.all():
+        await tariff_plan_service.create_tariff_plan(
+            date=datetime.date(2001, 1, 1), cargo_type='other', rate=0.04
+        )
